cb9a830bb3b3c7ccb3e99c8a8dcda4c996a1daf9
Morgan Lundin <morgan.lundin@laget.se>
2021-09-21 15:29:38 +0200

    InternalServerError exception added

---
+++ laget.Exceptions.Tests/InternalServerErrorExceptionTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using Xunit;
+
+namespace laget.Exceptions.Tests
+{
+    public class InternalServerErrorExceptionTest
+    {
+        [Fact]
+        public void ShouldCreateCorrectObject()
+        {
+            var message = "You cannot do this!";
+            var exception = new InternalServerErrorException(message);
+
+            Assert.Equal(message, exception.Message);
+            Assert.Equal(HttpStatusCode.InternalServerError, exception.StatusCode);
+            Assert.Equal("InternalServerErrorException", exception.Type);
+        }
+    }
+}
---
+++ laget.Exceptions/InternalServerErrorException.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace laget.Exceptions
+{
+    public class InternalServerErrorException : Exception
+    {
+        public override HttpStatusCode StatusCode => HttpStatusCode.InternalServerError;
+
+        public InternalServerErrorException(string message)
+            : base(message)
+        {
+        }
+
+        public InternalServerErrorException(string message, System.Exception ex)
+            : base(message, ex)
+        {
+        }
+    }
+}
