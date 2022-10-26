fb491c609c4c7156c2bd4e11e42c6947937df298
Erik Hennerfors <erik.hennerfors@laget.se>
2022-10-26 08:26:07 +0200

    Added ClientException and ValidationException since these are used in multiple projects and is not duplicated in all these projects

---
+++ laget.Exceptions/ClientException.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+
+namespace laget.Exceptions
+{
+    public class ClientException<T> : Exception
+    {
+        private static string ClientName => $"{typeof(T).Name}";
+
+        public override HttpStatusCode StatusCode => HttpStatusCode.InternalServerError;
+
+        public ClientException(string message)
+            : base($"{message} (Client='{ClientName}')")
+        {
+        }
+
+        public ClientException(string message, System.Exception ex)
+            : base($"{message} (Client='{ClientName}')", ex)
+        {
+        }
+
+        public ClientException(string message, object details)
+            : base($"{message} (Client='{ClientName}')")
+        {
+            Details = details;
+        }
+
+        public ClientException(string message, int? status, object details)
+            : base($"{message} (Client='{ClientName}')")
+        {
+            Details = details;
+            StatusCode = status != null ? (HttpStatusCode)status : HttpStatusCode.InternalServerError;
+        }
+    }
+}
---
+++ laget.Exceptions/ValidationException.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace laget.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public override HttpStatusCode StatusCode => HttpStatusCode.BadRequest;
+
+        public ValidationException(string message)
+            : base(message)
+        {
+        }
+
+        public ValidationException(string message, System.Exception exception)
+            : base(message, exception)
+        {
+        }
+
+        public ValidationException(string message, object details)
+            : base(message)
+        {
+            Details = details;
+        }
+
+        public ValidationException(IEnumerable<ValidationResult> result)
+            : base("Model validation failed")
+        {
+            Details = result;
+        }
+
+        public ValidationException(ValidationContext context, IEnumerable<ValidationResult> result)
+            : base($"Model validation failed for type {context.DisplayName}, please see details for further details.")
+        {
+            Details = result;
+        }
+    }
+}
