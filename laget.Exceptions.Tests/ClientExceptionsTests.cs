06ad9b1a729c5258a04b53d4d46028ce8461c7de
Erik Hennerfors <erik.hennerfors@laget.se>
2023-09-08 07:31:03 +0200

    Update(s)

---
+++ laget.Exceptions.Tests/ClientExceptionsTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using Xunit;
+
+namespace laget.Exceptions.Tests
+{
+    public class ClientExceptionsTests
+    {
+        [Fact]
+        public void ShouldCreateCorrectObject()
+        {
+            var message = "You cannot do this!";
+            var exception = new ClientException<SomeClient>(message)
+            {
+                StatusCode = HttpStatusCode.Conflict
+            };
+
+            Assert.Equal($"{message} (Client='SomeClient')", exception.Message);
+            Assert.Equal(HttpStatusCode.Conflict, exception.StatusCode);
+            Assert.IsType<ClientException<SomeClient>>(exception);
+        }
+
+        internal sealed class SomeClient
+        {
+        }
+    }
+}
